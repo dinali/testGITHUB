733d6bdffe84c6953fe426f66ed96ec5ae26ffa6
dinali <dinali@ers.usda.gov>
2013-02-11 16:05:04 -0500

    version 1
    added new tableViewController

---
+++ MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  testGITHUB
+//
+//  Created by Dina Li on 2/11/13.
+//  Copyright (c) 2013 USDA ERS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
